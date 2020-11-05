5fcdb270eb3a04583aa198bd089521e739da97ad
Diego Vaquero Melchor <diego.vaquero@wikifactory.com>
2020-11-05 09:09:07 +0100

    Fix: the creation of the tmp/outputs folder didn't work

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ import os
 app = FastAPI()
 app.include_router(manifests.router)
 
+TMP_FOLDER = "/tmp/outputs/"
 
 app.add_middleware(
     CORSMiddleware,
@@ -18,17 +19,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-TMP_FOLDER = "/tmp/outputs/"
+# Check if the /tmp/outputs folder exists
+try:
+    if not os.path.exists(TMP_FOLDER):
+        print("Creating tmp folder")
+        os.makedirs(TMP_FOLDER)
+
+except Exception as e:
+    print(e)
 
 if(__name__ == "__main__"):
-
-    # Check if the /tmp/outputs folder exists
-    try:
-        if not os.path.exists(TMP_FOLDER):
-            print("Creating tmp folder")
-            os.makedirs(TMP_FOLDER)
-
-    except Exception as e:
-        print(e)
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
