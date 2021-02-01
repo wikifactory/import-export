c35b67c96de0bf129104065af61359c3258f0c63
Diego Vaquero Melchor <diego.vaquero@wikifactory.com>
2021-02-01 13:16:46 +0100

    Fixed gitignore

--- app/tests/tests_config.py
+++ app/tests/tests_config.py
@@ -1,5 +1,5 @@
 test_db_name = "test"
 
 test_db_string = "postgresql://{}:{}@{}:{}/{}".format(
-    "ratata", "pika", "192.168.50.102", 8004, test_db_name
+    "wikifactory", "wikipass", "192.168.50.102", 8004, test_db_name
 )
