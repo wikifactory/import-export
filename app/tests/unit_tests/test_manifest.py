3f2872395002e9ff704650abaee12b3c6b111535
Diego Vaquero Melchor <diego.vaquero@wikifactory.com>
2021-02-11 15:47:15 +0100

    Added a test that creates a manifest from a mocked folder
    To check the process we use the gitimporter, since is the only one (for the moment) that generates it from a downloaded repository

--- app/tests/unit_tests/test_manifest.py
+++ app/tests/unit_tests/test_manifest.py
@@ -1,7 +1,12 @@
 from app.model.manifest import Manifest
 from app.model.element import Element, ElementType
-
+from app.models import add_job_to_db
+from app.controller.importers.git_importer import (
+    GitImporter,
+)
+from app.tests.integration_tests.test_job import create_job
 import pytest
+import os
 
 
 @pytest.fixture()
@@ -46,3 +51,70 @@ def test_plain_manifest(get_plain_manifest):
     manifest = get_plain_manifest
 
     assert len(manifest.elements) == 2
+
+
+def project_folder_generator(path):
+
+    folder_structure = [
+        ("{}".format(path), ["folderA", "folderB"], ["file3.txt"]),
+        ("{}/folderA".format(path), [], []),
+        ("{}/folderB".format(path), [], ["file1.png", "file2.csv"]),
+    ]
+
+    for row in folder_structure:
+        yield row
+
+
+def os_path_exits(path):
+    return True
+
+
+def test_mocked_folder_git(monkeypatch):
+
+    test_folder_name = "/testfolder"
+    monkeypatch.setattr(
+        os.path,
+        "exists",
+        os_path_exits,
+    )
+
+    monkeypatch.setattr(
+        os,
+        "walk",
+        project_folder_generator,
+    )
+
+    # Add the job to the DB
+    (job_id, job) = create_job()
+    add_job_to_db(job, job_id)
+
+    importer = GitImporter(job_id)
+
+    temp_manifest = Manifest()
+
+    importer.populate_manifest_from_repository_path(
+        temp_manifest, test_folder_name
+    )
+
+    root_element = temp_manifest.elements[0]
+
+    assert root_element.id == "root"
+    assert root_element.type == "2"
+
+    first_level_childrens = root_element.children
+
+    assert len(first_level_childrens) == 3  # folderA, folderB, file3
+
+    # Check the childrens of folderA
+    for el in first_level_childrens:
+        if el.path == "{}/folderA".format(test_folder_name):
+            assert len(el.children) == 0
+            assert el.type == "2"  # Of type folder
+            break
+
+    # Check the childrens of folderB
+    for el in first_level_childrens:
+        if el.path == "{}/folderB".format(test_folder_name):
+            assert len(el.children) == 2
+            assert el.type == "2"  # Of type folder
+            break
