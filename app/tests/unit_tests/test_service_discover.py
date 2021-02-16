697543f1bebbaa00e312f6ccdd71888fb02e76e8
Diego Vaquero Melchor <diego.vaquero@wikifactory.com>
2021-02-16 17:17:29 +0100

    Parametrized test_service_discover

--- app/tests/unit_tests/test_service_discover.py
+++ app/tests/unit_tests/test_service_discover.py
@@ -1,50 +1,23 @@
+import pytest
 from app.routers.service_discover import discover_service_for_url_list
 
 
-def test_url_list():
-
-    test_url_services = [
-        {
-            "url": "https://www.dropbox.com/home/folder/otherfolder/file",
-            "service": "dropbox",
-        },
-        {
-            "url": "http://www.dropbox.com/home/anyfolder/anotherone",
-            "service": "dropbox",
-        },
-        {
-            "url": "https://drive.google.com/drive/u/0/folders/1-123456sYN8jsTeEpqrr5hSB0adKasdf",
-            "service": "googledrive",
-        },
-        {
-            "url": "https://github.com/user/repo",
-            "service": "git",
-        },
-        {
-            "url": "ssh://git@github.com/<user>/<repository name>.git",
-            "service": "unknown",
-        },
-        {
-            "url": "ssh://git@github.com/user/repository_name.git",
-            "service": "git",
-        },
-        {"url": "www.com", "service": "unknown"},
-        {
-            "url": "https://www.thingiverse.com/thing:1234567",
-            "service": "unknown",
-        },
-    ]
-
-    only_urls = []
-    for r in test_url_services:
-        only_urls.append(r["url"])
-
-    discovered_service = discover_service_for_url_list(only_urls)
-
-    for url in only_urls:
-
-        right_service = [
-            x["service"] for x in test_url_services if x["url"] == url
-        ][0]
-
-        assert right_service == discovered_service[url]
+@pytest.mark.parametrize(
+    "url, right_service",
+    [
+        ("https://www.dropbox.com/home/folder/otherfolder/file", "dropbox"),
+        ("http://www.dropbox.com/home/anyfolder/anotherone", "dropbox"),
+        (
+            "https://drive.google.com/drive/u/0/folders/1-123456sYN8jsTeEpqrr5hSB0adKasdf",
+            "googledrive",
+        ),
+        ("https://github.com/user/repo", "git"),
+        ("ssh://git@github.com/<user>/<repository name>.git", "unknown"),
+        ("ssh://git@github.com/user/repository_name.git", "git"),
+        ("www.com", "unknown"),
+        ("https://www.thingiverse.com/thing:1234567", "unknown"),
+    ],
+)
+def test_service_discover(url, right_service):
+    discovered_service = discover_service_for_url_list([url])
+    assert right_service == discovered_service[url]
