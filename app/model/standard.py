46a6fcee6e6b6939debb7a97f3ea64689b13afd0
Andrés Barreiro <andres.bga@gmail.com>
2021-02-20 18:49:53 +0100

    Remove Certification

--- app/model/standard.py
+++ app/model/standard.py
@@ -1,10 +1,6 @@
-# from certification import Certification
 import json
-from typing import List
 
 from pydantic.dataclasses import dataclass
-
-from app.model.certification import Certification
 
 
 @dataclass
@@ -13,7 +9,6 @@ class Standard:
     id: str = ""
     title: str = ""
     reference: str = ""
-    certifications: List[Certification] = []
 
     def toJson(self):
         return json.dumps(self, default=lambda o: o.__dict__)
