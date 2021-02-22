913168cae4771ca641ceed4141e4f624711ea627
Andrés Barreiro <andres.bga@gmail.com>
2021-02-22 13:04:23 +0100

    Use filter_by as get won't work on non-PK job_id

--- app/crud/crud_manifest.py
+++ app/crud/crud_manifest.py
@@ -7,7 +7,7 @@ from app.schemas import ManifestInput
 
 class CRUDManifest(CRUDBase[Manifest, ManifestInput, ManifestInput]):
     def create_or_update(self, db: Session, *, obj_in: ManifestInput) -> Manifest:
-        db_manifest = db.query(Manifest).get(job_id=obj_in.job_id)
+        db_manifest = db.query(Manifest).filter_by(job_id=obj_in.job_id).one_or_none()
 
         if db_manifest:
             self.update(db, db_obj=db_manifest, obj_in=obj_in)
