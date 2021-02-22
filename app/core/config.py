e190403203c0d200150ecb270c2678bb9141e912
Andrés Barreiro <andres.bga@gmail.com>
2021-02-22 12:59:17 +0100

    DOWNLOAD_BASE_PATH value for tests

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from typing import Any, Dict, List, Optional, Union
 
@@ -58,6 +59,15 @@ class Settings(BaseSettings):
 
     # TODO - define validations
     DOWNLOAD_BASE_PATH: str
+
+    @validator("DOWNLOAD_BASE_PATH", pre=True)
+    def ensure_download_path(cls, v: str) -> str:
+        if "pytest" in sys.modules:
+            current_dir = os.path.dirname(os.path.realpath(__file__))
+            return os.path.normpath(os.path.join(current_dir, "../tests/test_files"))
+        # if directory can't be written, raise ValueError(v)
+        return v
+
     BROKER_URL: str
     BACKEND_URL: str
 
