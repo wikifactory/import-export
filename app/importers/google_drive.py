ec37ba1044b763ab4c2c3389eaca9c56e412b649
Andrés Barreiro <andres.bga@gmail.com>
2021-02-23 21:55:12 +0100

    GoogleDriveImporter fixes and basic tests

--- app/importers/google_drive.py
+++ app/importers/google_drive.py
@@ -4,17 +4,26 @@ from re import search
 
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+from pydrive.files import ApiRequestError, FileNotDownloadableError
 from sqlalchemy.orm import Session
 
 from app import crud
 from app.importers.base import BaseImporter
 from app.models.job import JobStatus
+from app.schemas import ManifestInput
 
-googledrive_folder_regex = r"^(https:\/\/drive\.google\.com\/drive\/(u\/[0-9]+\/)?folders\/.*(\?usp=sharing)?$)"
+googledrive_folder_regex = r"^https?:\/\/drive\.google\.com\/drive\/(u\/[0-9]+\/)?folders\/(?P<folder_id>[-\w]{25,})"
+
+folder_mimetype = "application/vnd.google-apps.folder"
 
 
 def is_folder(item):
-    item.get("mimeType") == "application/vnd.google-apps.folder"
+    return item.get("mimeType") == folder_mimetype
+
+
+def folder_id_from_url(url):
+    match = search(googledrive_folder_regex, url)
+    return match.group("folder_id")
 
 
 class GoogleDriveImporter(BaseImporter):
@@ -24,27 +33,27 @@ class GoogleDriveImporter(BaseImporter):
         self.drive = None
         self.tree_root = {"item": None, "children": {}}
 
-    def validate_url(url):
+    def validate_url(self, url):
         return bool(search(googledrive_folder_regex, url))
 
-    def build_tree_recursively(self, folder_id, current_level):
+    def build_tree_recursively(self, current_level, folder_id):
         item_list = self.drive.ListFile(
             {"q": f"'{folder_id}' in parents and trashed=false"}
         ).GetList()
 
         for item in item_list:
-            name = item.get("name")
-            current_level.set(name, {"item": item, "children": {}})
+            name = item.get("title")
+            current_level[name] = {"item": item, "children": {}}
 
-        for (_, node) in current_level:
+        for node in current_level.values():
             item = node.get("item")
             if is_folder(item):
-                self.process_folder_recursively(item.get("id"), node.get("children"))
+                self.build_tree_recursively(node.get("children"), item.get("id"))
 
     def download_tree_recursively(self, current_level, accumulated_path):
         Path(accumulated_path).mkdir(parents=True, exist_ok=True)
 
-        for (name, node) in current_level:
+        for (name, node) in current_level.items():
             item = node.get("item")
             item_full_path = os.path.join(accumulated_path, name)
             if is_folder(item):
@@ -53,19 +62,48 @@ class GoogleDriveImporter(BaseImporter):
                 item.GetContentFile(item_full_path)
 
     def process(self):
-        job = crud.job.get(self.db)
+        job = crud.job.get(self.db, self.job_id)
         crud.job.update_status(self.db, db_obj=job, status=JobStatus.IMPORTING)
 
-        # FIXME - proper auth
         gauth = GoogleAuth()
+        # FIXME - for security and trust purposes, it is probably better
+        # to use the "drive.file" scope (because "drive.readonly" can read the full Drive contents).
+        # As a downside, it needs the private folders to be shared with the app in advance.
+        gauth.settings["oauth_scope"] = "https://www.googleapis.com/auth/drive.readonly"
+
+        if job.import_token:
+            gauth.Auth(job.import_token)
+
         self.drive = GoogleDrive(gauth)
 
-        self.build_tree_recursively(self.tree_root, job.import_url)
-        # FIXME - this can probably be improved by flattening the tree first
-        # and then starting downloads in parallel
-        self.download_tree_recursively(self.tree_root, job.path)
+        folder_id = folder_id_from_url(job.import_url)
 
-        # TODO - populate manifest
+        try:
+            res = self.drive.ListFile(
+                {"q": f"'{folder_id}' and trashed=false"}
+            ).GetList()
+
+            [root_folder] = res
+
+            self.tree_root["item"] = root_folder
+
+            self.build_tree_recursively(self.tree_root["children"], folder_id)
+            # FIXME - this can probably be improved by flattening the tree first
+            # and then starting downloads in parallel
+            self.download_tree_recursively(self.tree_root["children"], job.path)
+        except (ApiRequestError, FileNotDownloadableError):
+            # FIXME - store/pass this API clients can use it
+            # auth_url = gauth.GetAuthUrl()
+            crud.job.update_status(
+                self.db,
+                db_obj=job,
+                status=JobStatus.IMPORTING_ERROR_DATA_UNREACHABLE,
+            )
+            return
+
+        manifest_input = ManifestInput(job_id=job.id, source_url=job.import_url)
+        manifest_input.project_name = self.tree_root.get("item").get("title")
+        # TODO - add project_description to manifest
 
         crud.manifest.update_or_create(self.db, obj_in=manifest_input)
         crud.job.update_status(
--- app/tests/importers/test_google_drive.py
+++ app/tests/importers/test_google_drive.py
@@ -1,185 +1,287 @@
-import uuid
+import os
+import shutil
+from re import search
 
-import oauth2client
 import pytest
-from googleapiclient.errors import HttpError
+from pydrive.drive import GoogleDrive
+from pydrive.files import ApiRequestError, FileNotDownloadableError, GoogleDriveFile
+from sqlalchemy.orm import Session
 
-from app.controller.importers.googledrive_errors import (
-    CredentialsNotValid,
-    DownloadError,
-)
-from app.controller.importers.googledrive_importer import GoogleDriveImporter
-from app.models import add_job_to_db
+from app import crud
+from app.core.config import settings
+from app.importers.google_drive import GoogleDriveImporter, folder_mimetype, is_folder
+from app.models.job import JobStatus
+from app.models.job_log import JobLog
+from app.schemas import JobCreate
+from app.tests.utils import utils
+
+
+class GoogleDriveFileListMock:
+    def GetList(self):
+        return [
+            GoogleDriveFile(metadata=file_metadata, uploaded=True)
+            for file_metadata in self.mocked_list
+        ]
 
 
 @pytest.fixture
-def basic_job():
-    options = {
-        "import_service": "git",
-        "import_url": "https://github.com/wikifactory/sample-project",
-        "import_token": None,
-        "export_service": "wikifactory",
-        "export_url": "https://wikifactory.com/@user/test-project",
-        "export_token": "---------",
+def pydrive_mock(monkeypatch, remote_data: dict):
+    def mock_list_file(self, param={}):
+        match = search(r"'(?P<item_id>[-\w]+)'(?P<in_parents> in parents)?", param["q"])
+        item_id = match.group("item_id")
+        get_children = bool(match.group("in_parents"))
+
+        list_object = GoogleDriveFileListMock()
+
+        if get_children:
+            list_object.mocked_list = remote_data[item_id].get("children", [])
+        else:
+            item = remote_data[item_id].get("item")
+            list_object.mocked_list = [item] if item else []
+
+        return list_object
+
+    monkeypatch.setattr(GoogleDrive, "ListFile", mock_list_file)
+
+
+@pytest.fixture
+def download_mock(monkeypatch, basic_job):
+    tmp_directory = basic_job["db_job"].path
+
+    def mock_get_content(self, download_path):
+        assert download_path.startswith(tmp_directory)
+        with open(download_path, "wb") as file_handle:
+            file_handle.write(b"dummy")
+
+    monkeypatch.setattr(GoogleDriveFile, "GetContentFile", mock_get_content)
+    yield
+    shutil.rmtree(tmp_directory, ignore_errors=True)
+
+
+@pytest.fixture(scope="function")
+def basic_job(db: Session) -> dict:
+    random_folder_id = utils.random_lower_string()
+    job_input = JobCreate(
+        import_service="google_drive",
+        import_url=f"https://drive.google.com/drive/u/0/folders/{random_folder_id}",
+        export_service="wikifactory",
+        export_url=f"https://wikifactory.com/@user/{random_folder_id}",
+    )
+    db_job = crud.job.create(db, obj_in=job_input)
+    db_job.path = os.path.join(settings.DOWNLOAD_BASE_PATH, random_folder_id)
+    yield {
+        "job_input": job_input,
+        "db_job": db_job,
+        "folder_id": random_folder_id,
     }
-    job_id = str(uuid.uuid4())
-    add_job_to_db(options, job_id)
-    return (job_id, options)
+    crud.job.remove(db, id=db_job.id)
 
 
-class FakeServiceClassSuccess:
-    class FileList:
-        class ExecuteObject:
-            class FakeFile:
-                info = {"name": "", "id": "", "mimeType": ""}
-
-                def __init__(self, name="", id="", mimeType="text/plain"):
-                    self.info["name"] = name
-                    self.info["id"] = id
-
-                def get(self, key):
-                    return self.info[key]
-
-            def execute(self):
-                return {
-                    "files": [self.FakeFile("testname", "testid")],
-                    "nextPageToken": None,
-                }
-
-        def list(self, q, fields):
-            return self.ExecuteObject()
-
-        def get_media(self, fileId):
-            pass
-
-    def files(self):
-        return self.FileList()
-
-
-class FakeServiceClassFail:
-    class FileList:
-        class ExecuteObject:
-            def execute(self):
-                raise oauth2client.client.AccessTokenCredentialsError
-
-        def list(self, q, fields):
-            return self.ExecuteObject()
-
-    def files(self):
-        return self.FileList()
-
-
-def patched_googledrive_service_from_doc_success(
-    service,
-    base,
-    http,
-    developerKey,
-    model,
-    requestBuilder,
-    credentials,
-    client_options,
-    adc_cert_path,
-    adc_key_path,
-):
-    return FakeServiceClassSuccess()
-
-
-def patched_googledrive_service_from_doc_auth_error(
-    service,
-    base,
-    http,
-    developerKey,
-    model,
-    requestBuilder,
-    credentials,
-    client_options,
-    adc_cert_path,
-    adc_key_path,
-):
-    return FakeServiceClassFail()
+@pytest.mark.parametrize(
+    "remote_data, expected_tree",
+    [
+        (
+            {
+                "root-folder": {
+                    "item": {"mimeType": folder_mimetype},
+                    "children": [
+                        {
+                            "id": "root-file-1",
+                            "title": "README.md",
+                            "mimeType": "text/plain",
+                        },
+                        {
+                            "id": "subfolder-1",
+                            "title": "Subfolder",
+                            "mimeType": folder_mimetype,
+                        },
+                    ],
+                },
+                "subfolder-1": {
+                    "item": {"mimeType": folder_mimetype},
+                    "children": [
+                        {
+                            "id": "nested-file-1",
+                            "title": "test.txt",
+                            "mimeType": "text/plain",
+                        },
+                    ],
+                },
+            },
+            {
+                "README.md": {
+                    "item": {
+                        "id": "root-file-1",
+                        "title": "README.md",
+                        "mimeType": "text/plain",
+                    },
+                    "children": {},
+                },
+                "Subfolder": {
+                    "item": {
+                        "id": "subfolder-1",
+                        "title": "Subfolder",
+                        "mimeType": folder_mimetype,
+                    },
+                    "children": {
+                        "test.txt": {
+                            "item": {
+                                "id": "nested-file-1",
+                                "title": "test.txt",
+                                "mimeType": "text/plain",
+                            },
+                            "children": {},
+                        }
+                    },
+                },
+            },
+        )
+    ],
+)
+@pytest.mark.usefixtures("pydrive_mock")
+def test_build_tree_recursively(db: Session, basic_job: dict, expected_tree: dict):
+    importer = GoogleDriveImporter(db, basic_job["db_job"].id)
+    importer.drive = GoogleDrive()
+    tree = {}
+    importer.build_tree_recursively(tree, "root-folder")
+    assert tree == expected_tree
 
 
-def patchedMediaIOBaseDownloadSuccess(fd, request):
-    class FakeDownloader:
-        def next_chunk():
-            return (None, True)
-
-    return FakeDownloader
-
-
-def patchedMediaIOBaseDownloadFail(fd, request):
-    class FakeDownloader:
-        def next_chunk():
-            raise HttpError(None, b"error")
-
-    return FakeDownloader
+def assert_tree_directory_recursive(current_level, accumulated_path):
+    for (name, node) in current_level.items():
+        check_path = os.path.join(accumulated_path, name)
+        item = node.get("item")
+        if is_folder(item):
+            assert os.path.isdir(check_path)
+            children = node.get("children")
+            assert_tree_directory_recursive(children, check_path)
+        else:
+            assert os.path.isfile(check_path)
 
 
-def monkeypatched_googledrive_files_fail(url, path, callbacks):
-    raise oauth2client.client.AccessTokenCredentialsError
+@pytest.mark.parametrize(
+    "tree",
+    [
+        {
+            "README.md": {
+                "item": GoogleDriveFile(
+                    metadata={
+                        "id": "root-file-1",
+                        "title": "README.md",
+                        "mimeType": "text/plain",
+                    },
+                    uploaded=True,
+                ),
+                "children": {},
+            },
+            "Subfolder": {
+                "item": GoogleDriveFile(
+                    metadata={
+                        "id": "subfolder-1",
+                        "title": "Subfolder",
+                        "mimeType": folder_mimetype,
+                    },
+                    uploaded=True,
+                ),
+                "children": {
+                    "test.txt": {
+                        "item": GoogleDriveFile(
+                            metadata={
+                                "id": "nested-file-1",
+                                "title": "test.txt",
+                                "mimeType": "text/plain",
+                            },
+                            uploaded=True,
+                        ),
+                        "children": {},
+                    }
+                },
+            },
+        },
+    ],
+)
+@pytest.mark.usefixtures("download_mock")
+def test_download_tree_recursively(db: Session, basic_job: dict, tree: dict):
+    importer = GoogleDriveImporter(db, basic_job["db_job"].id)
+    importer.drive = GoogleDrive()
+    importer.download_tree_recursively(tree, basic_job["db_job"].path)
+    assert_tree_directory_recursive(tree, basic_job["db_job"].path)
 
 
 @pytest.fixture
-def patch_build_from_doc(monkeypatch):
-    monkeypatch.setattr(
-        "googleapiclient.discovery.build_from_document",
-        patched_googledrive_service_from_doc_success,
+def remote_data(basic_job: dict):
+    folder_id = basic_job["folder_id"]
+
+    return {
+        folder_id: {
+            "item": {
+                "id": folder_id,
+                "title": "My Google Drive Project",
+                "mimeType": folder_mimetype,
+            },
+            "children": [
+                {
+                    "id": "root-file-1",
+                    "title": "README.md",
+                    "mimeType": "text/plain",
+                },
+                {
+                    "id": "subfolder-1",
+                    "title": "Subfolder",
+                    "mimeType": folder_mimetype,
+                },
+            ],
+        },
+        "subfolder-1": {
+            "item": {
+                "id": "subfolder-1",
+                "title": "Subfolder",
+                "mimeType": folder_mimetype,
+            },
+            "children": [
+                {
+                    "id": "nested-file-1",
+                    "title": "test.txt",
+                    "mimeType": "text/plain",
+                },
+            ],
+        },
+    }
+
+
+@pytest.mark.usefixtures("pydrive_mock", "download_mock")
+def test_google_drive_importer(db: Session, basic_job: dict, remote_data: dict):
+    job = basic_job["db_job"]
+    importer = GoogleDriveImporter(db, basic_job["db_job"].id)
+    importer.process()
+    assert_tree_directory_recursive(
+        importer.tree_root["children"], basic_job["db_job"].path
     )
+    importing_status_log = (
+        db.query(JobLog).filter_by(job_id=job.id, to_status=JobStatus.IMPORTING).one()
+    )
+    assert importing_status_log
+    assert job.status is JobStatus.IMPORTING_SUCCESSFULLY
+    assert job.manifest
+    assert (
+        job.manifest.project_name
+        == remote_data[basic_job["folder_id"]]["item"]["title"]
+    )
+    assert job.manifest.source_url == job.import_url
 
 
 @pytest.fixture
-def patch_download_success(monkeypatch):
-    # Monkeypatch the download
-    monkeypatch.setattr(
-        "app.controller.importers.googledrive_importer.MediaIoBaseDownload",
-        patchedMediaIOBaseDownloadSuccess,
-    )
+def api_error(monkeypatch, remote_data: dict, exception: Exception):
+    def mock_list_file(self, param={}):
+        raise exception()
+
+    monkeypatch.setattr(GoogleDrive, "ListFile", mock_list_file)
 
 
-@pytest.fixture
-def patch_download_error(monkeypatch):
-    # Monkeypatch the download
-    monkeypatch.setattr(
-        "app.controller.importers.googledrive_importer.MediaIoBaseDownload",
-        patchedMediaIOBaseDownloadFail,
-    )
-
-
-def test_googledrive_manifest_generation_success(
-    patch_build_from_doc, patch_download_success, basic_job
-):
-
-    (job_id, details) = basic_job
-
-    importer = GoogleDriveImporter(job_id)
-
-    manifest = importer.process_url(details["import_url"], details["import_token"])
-
-    assert manifest is not None
-
-
-def test_googledrive_manifest_generation_error_downloading(
-    patch_build_from_doc, patch_download_error, basic_job
-):
-
-    (job_id, details) = basic_job
-
-    importer = GoogleDriveImporter(job_id)
-
-    with pytest.raises(DownloadError):
-        _ = importer.process_url(details["import_url"], details["import_token"])
-
-
-def test_googledrive_manifest_generation_credentials_not_valid(monkeypatch, basic_job):
-
-    monkeypatch.setattr(
-        "googleapiclient.discovery.build_from_document",
-        patched_googledrive_service_from_doc_auth_error,
-    )
-
-    (job_id, details) = basic_job
-
-    importer = GoogleDriveImporter(job_id)
-
-    with pytest.raises(CredentialsNotValid):
-        importer.process_url(details["import_url"], details["import_token"])
+@pytest.mark.parametrize("exception", [ApiRequestError, FileNotDownloadableError])
+@pytest.mark.usefixtures("api_error")
+def test_google_drive_importer_api_error(db: Session, basic_job: dict):
+    job = basic_job["db_job"]
+    importer = GoogleDriveImporter(db, basic_job["db_job"].id)
+    importer.process()
+    assert job.status is JobStatus.IMPORTING_ERROR_DATA_UNREACHABLE
