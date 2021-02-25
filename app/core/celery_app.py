2b6ad88654d9f70142c874fde4854d0071a5ff9d
Andrés Barreiro <andres.bga@gmail.com>
2021-02-25 17:05:28 +0100

    Add back broker param to celery

--- app/core/celery_app.py
+++ app/core/celery_app.py
@@ -3,5 +3,7 @@ from celery import Celery
 from app.core.config import settings
 
 celery_app = Celery(
-    "import_export_tasks", task_always_eager=not bool(settings.BROKER_URL)
+    "import_export_tasks",
+    broker=settings.BROKER_URL,
+    task_always_eager=not bool(settings.BROKER_URL),
 )
